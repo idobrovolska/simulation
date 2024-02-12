d85a38c6356062edcab859cc1204ec94ff9b6674
Yuliia <juliadenisyuk2006@gmail.com>
2024-02-12 17:58:01 +0200

    utils yulla

--- utils.py
+++ utils.py
@@ -4,3 +4,14 @@ def factorial(n):
 
 
 	return fact
+
+def ncd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+num1 = int(input())
+num2 = int(input())
+
+result = ncd(num1, num2)
+print(result)
